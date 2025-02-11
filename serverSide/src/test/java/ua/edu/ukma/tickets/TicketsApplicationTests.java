692a92b88dc2f5123ad3e319ea4b958a03afe189
Andrii Mazurok <andy.mazurok@gmail.com>
2025-02-11 21:39:41 +0200

    Init for server side app

---
+++ serverSide/src/test/java/ua/edu/ukma/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
